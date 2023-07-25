5009535dc364851819d9a9ff99a3d28b310304ee
Rens Heddes <rens@easy-lms.com>
2023-07-25 11:27:00 +0200

    Introduce alternative test case

--- src/client/roasters/Roasters.spec.tsx
+++ src/client/roasters/Roasters.spec.tsx
@@ -1,22 +1,30 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, waitFor } from '@testing-library/react';
 import { describe, it, expect } from 'vitest';
 import Roasters, { RoasterList } from './Roasters';
 
 describe('Roasters', () => {
   it('shows no roasters', async () => {
     const { container } = render(<RoasterList roasters={[]} />);
-
     expect(container).to.be.empty;
   });
 
-  it('shows text "Hello World"', async () => {
+  it('shows text the "Hello World" coffee roaster', async () => {
     const fetchRoasters = async () => ({
       roasters: [{ code: 'example-roaster', title: 'Hello World' }],
     });
-
     const { findByText } = render(<Roasters fetchRoasters={fetchRoasters} />);
-
     expect(await findByText('Hello World')).to.exist;
+  });
+
+  it('still shows the "Hello Coffee" roaster', async () => {
+    const fetchRoasters = async () => ({
+      roasters: [{ code: 'example-roaster', title: 'Hello Coffee' }],
+    });
+    const wrapper = render(<Roasters fetchRoasters={fetchRoasters} />);
+    await waitFor(() => {
+      expect(wrapper.container.children).to.have.length(1);
+      expect(wrapper.container.innerHTML).to.equal('<span>Hello Coffee</span>');
+    });
   });
 });
